13b1872be9c0bdf99147111bfeb845b91d38aa10
My <502914391@qq.com>
2019-12-04 23:28:55 +0800

    添加权限管理导航

--- web/js/index.js
+++ web/js/index.js
@@ -8,13 +8,24 @@ layui.use(['bodyTab', 'form', 'element', 'layer', 'jquery'], function () {
     var form = layui.form,
         element = layui.element;
     $ = layui.$;
+
+    var right = window.sessionStorage.getItem("right");
+    var url;
+    if (right == 1)
+
+        url = "json/teacher.json";
+    else if (right == 2)
+        url = "json/student.json";
+    else
+        url = "json/admin.json";
     layer = parent.layer === undefined ? layui.layer : top.layer;
     tab = layui.bodyTab({
         openTabNum: "50",  //最大可打开窗口数量
-        url: "json/navs.json" //获取菜单json地址
+        url: url//获取菜单json地址
     });
 
     $(".userName").text(window.sessionStorage.getItem("user"));
+    $(".adminName").text(window.sessionStorage.getItem("user"));
 
 
     //通过顶部菜单获取左侧二三级菜单   注：此处只做演示之用，实际开发中通过接口传参的方式获取导航数据
